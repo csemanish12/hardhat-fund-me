c2c0703a6855196ac05e5c9b7753483d21304d40
Manish Sah <sahmanish20@gmail.com>
2023-06-06 08:00:15 +0545

    create helper hardhat config for handling different chainlinks

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -4,12 +4,22 @@
 
 // module.exports.default = deployFunc
 
+const { networkConfig } = require("../helper-hardhat-config")
+
 module.exports = async (hre) => {
     const { getNamedAccounts, deployments } = hre
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
     const chainId = network.config.chainId
 
+    const ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+
+
     // when going for localhost or hardhat network we want to use a mock
     // what happens when we want to change chains?
+    const fundMe = await deploy("FundMe", {
+        from: deployer,
+        args: [],
+        log: true,
+    })
 }
