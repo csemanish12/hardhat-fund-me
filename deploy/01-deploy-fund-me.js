079bac9771c177a95c00e1a5f9eb5e267a4b640f
Manish Sah <sahmanish20@gmail.com>
2023-06-07 09:28:17 +0545

    create verfy utils

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -5,15 +5,16 @@
 // module.exports.default = deployFunc
 
 const { networkConfig, developmentChains } = require("../helper-hardhat-config")
-const {network} = require("hardhat")
+const { network } = require("hardhat")
 module.exports = async (hre) => {
     const { getNamedAccounts, deployments } = hre
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
+    const { verify } = require("../utils/verify")
     const chainId = network.config.chainId
 
-    let ethUsdPriceFeedAddress 
-    if(developmentChains.includes(network.name)){
+    let ethUsdPriceFeedAddress
+    if (developmentChains.includes(network.name)) {
         const ethUsdAggregator = await deployments.get("MockV3Aggregator")
         ethUsdPriceFeedAddress = ethUsdAggregator.address
     } else {
@@ -24,11 +25,19 @@ module.exports = async (hre) => {
 
     // when going for localhost or hardhat network we want to use a mock
     // what happens when we want to change chains?
+    const args = [ethUsdPriceFeedAddress]
     const fundMe = await deploy("FundMe", {
         from: deployer,
-        args: [ethUsdPriceFeedAddress],
+        args: args,
         log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
     })
+    if (
+        !developmentChains.includes(network.name) &&
+        process.env.ETHERSCAN_API_KEY
+    ) {
+        await verify(fundMe.address, args)
+    }
     log("-------------------------------------------------")
 }
 module.exports.tags = ["all", " fundme"]
--- hardhat.config.js
+++ hardhat.config.js
@@ -20,6 +20,7 @@ module.exports = {
             url: SEPOLIA_RPC_URL,
             accounts: [PRIVATE_KEY],
             chainId: 11155111,
+            blockConfirmations: 6
         },
         localhost: {
             url: "http://127.0.0.1:8545",
