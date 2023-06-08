64e25186af332cba34fb15a3d8d22051f1d8a289
Manish Sah <sahmanish20@gmail.com>
2023-06-08 21:27:23 +0545

    add unit test for withdraw function

--- test/unit/FundMe.test.js
+++ test/unit/FundMe.test.js
@@ -37,10 +37,47 @@ describe("FundMe", async function () {
             assert.equal(response.toString(), sendValue.toString())
         })
 
-        it("adds funder to array of funders", async function(){
-            await fundMe.fund({value: sendValue})
+        it("adds funder to array of funders", async function () {
+            await fundMe.fund({ value: sendValue })
             const funder = await fundMe.funders(0)
             assert.equal(funder, deployer)
+        })
+    })
+
+    describe("withdraw", async function () {
+        beforeEach(async function () {
+            await fundMe.fund({ value: sendValue })
+        })
+
+        it("withdraw eth from a single founder", async function () {
+            // arrange
+            const startingFundMeBalance = await fundMe.provider.getBalance(
+                fundMe.address
+            )
+            const startingDeployerBalance = await fundMe.provider.getBalance(
+                deployer
+            )
+
+            // act
+            const transactionResponse = await fundMe.withdraw()
+            const transactionReceipt = await transactionResponse.wait(1)
+
+            const {gasUsed, effectiveGasPrice} = transactionReceipt
+            const gasCost = gasUsed.mul(effectiveGasPrice)
+
+            const endingFundMeBalance = await fundMe.provider.getBalance(
+                fundMe.address
+            )
+            const endingDeployerBalance = await fundMe.provider.getBalance(
+                deployer
+            )
+
+            // assert
+            assert.equal(endingFundMeBalance, 0)
+            assert.equal(
+                startingFundMeBalance.add(startingDeployerBalance).toString(),
+                endingDeployerBalance.add(gasCost).toString()
+            )
         })
     })
 })
